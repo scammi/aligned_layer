b4ac612dc815160b176a89c7dd66d14beeb2196a
scammi <scammi@gmail.com>
2024-04-17 18:16:02 -0300

    Mina client

--- operator/hello.js
+++
@@ -1,8 +0,0 @@
-console.log('HEY !!!!!')
-
-
-// Access command-line arguments
-const args = process.argv.slice(2);
-
-// Print the arguments
-console.log("Arguments:", args);
---
+++ operator/mina/mina_client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Pokemon struct {
+	Name string `json:"name"`
+	// Add more fields as needed
+}
+
+func main() {
+	// Make the HTTP GET request to the Pokémon API
+	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/ditto")
+	if err != nil {
+		fmt.Println("Error making HTTP request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check if the response status code is OK (200)
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: unexpected status code:", resp.StatusCode)
+		return
+	}
+
+	// Decode the response body into a Pokemon struct
+	var pokemon Pokemon
+	if err := json.NewDecoder(resp.Body).Decode(&pokemon); err != nil {
+		fmt.Println("Error decoding response body:", err)
+		return
+	}
+
+	// Print the name of the Pokémon
+	fmt.Println("Name:", pokemon.Name)
+	// Add more fields printing or processing as needed
+}
