6556def82d2478e87da32c6882130023015b6e7a
scammi <scammi@gmail.com>
2024-04-18 18:27:51 -0300

    read encoded files

--- mina/index.ts
+++ mina/index.ts
@@ -1,27 +1,24 @@
-// Import the required modules
-// import * as proof from './proof.json';
-// import * as verification from './verification.json';
-
 import express, { Request, Response } from 'express';
 import { Field, verify } from 'o1js';
-var msgpack = require("msgpack-lite");
+
+const fs = require('fs');
+const msgpack = require("msgpack-lite");
 
 const app = express();
 
 // Define a route to handle GET requests to the root URL
-app.post('/', async (req: Request<{}, {}, { proof: Buffer, verification: Buffer }>, res: Response) => {
-    const proofDecoded = msgpack.decode(req.body.proof);
-    const verificationDecoded = msgpack.decode(req.body.verification);
+app.get('/', async (req: Request, res: Response) => {
+    const proof = await fs.promises.readFile('./proofs/proof.bin');
+    const verification = await fs.promises.readFile('./proofs/verification.bin');
+
+    const proofDecoded = msgpack.decode(proof);
+    const verificationDecoded = msgpack.decode(verification);
     
     const verificationBuffer = JSON.parse(JSON.stringify(verificationDecoded));
-
     verificationBuffer.hash = Field.fromJSON(verificationDecoded.hash);
 
-    console.log('>>>> verifying')
-
-    // const ok = await verify(proofDecoded, verificationBuffer);
-
-    console.log('>>>> ', true)
+    console.log('>>>> verifying', verificationBuffer, proofDecoded)
+    const ok = await verify(proofDecoded, verificationBuffer);
 
     res.send({ success: true });
 });
